3d231f1bbb037297c29977a698c088abdff71ad9
Lazar Stojanović <lakisd98@gmail.com>
2024-01-08 23:47:09 +0100

    Add: shadcn/ui & max width wrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
 export default function Home() {
-	return <h1>Hello</h1>;
+	return (
+		<MaxWidthWrapper>
+			<h1>Hello</h1>
+		</MaxWidthWrapper>
+	);
 }
